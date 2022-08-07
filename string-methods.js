86c540881216868bafb89bd89ac6e2c0201e1c76
Gift13-07 <giftchinwendu07@gmail.com>
2022-08-07 15:25:58 +0600

    git commit

---
+++ string-methods.js
@@ -0,0 +1,27 @@
+let sample = 'Unveil';
+console.log(sample.substring(2));
+
+let greeting = "       Good Morning       ";
+console.log(greeting);
+console.log(greeting.trim());
+
+let quote="Nothing good comes easy.";
+console.log(quote.toUpperCase())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
