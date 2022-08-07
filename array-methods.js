9dbe7dd0bbdc4efb5526d9045b370e049021c14e
Gift13-07 <giftchinwendu07@gmail.com>
2022-08-07 14:41:04 +0600

    git commit

---
+++ array-methods.js
@@ -0,0 +1,12 @@
+let names= ['Jessica', 'Ella', 'Mboyi']
+console.log(names.join())
+console.log(names.join(''))
+console.log(names.join('-'))
+
+let numbers= [89,53,1,99,70]
+numbers.sort()
+console.log(numbers)
+
+let words=['beauty','calm','mercy','prudent','love']
+words.reverse()
+console.log(words)
