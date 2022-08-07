53a04f31b04566209c63819b17cb9737b75e6542
Gift13-07 <giftchinwendu07@gmail.com>
2022-08-07 15:38:08 +0600

    git commit

--- string-methods.js
+++ string-methods.js
@@ -1,5 +1,6 @@
-let sample = 'Unveil';
-console.log(sample.substring(2));
+let sample = 'The cat fell on the fence during escape.';
+console.log(sample.slice(16));
+console.log(sample.slice(-7))
 
 let greeting = "       Good Morning       ";
 console.log(greeting);
@@ -8,20 +9,5 @@ console.log(greeting.trim());
 let quote="Nothing good comes easy.";
 console.log(quote.toUpperCase())
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+let note="HASTA LA VISTA"
+console.log(note.toLowerCase())
